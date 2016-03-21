de9cae2b1b9715a84b1ba847ef5bb871d45007db
Wesley Hurd <wesley@wesleyhurd.com>
2016-03-21 14:42:42 -0500

    Updating nginx configuration to use symbolic links

--- VagrantScripts/nginx_configure.sh
+++ VagrantScripts/nginx_configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /vagrant/VagrantScripts/vagrant-nginx.conf /etc/nginx/sites-enabled/vagrant-nginx.conf
+sudo ln -s /vagrant/VagrantScripts/vagrant-nginx.conf /etc/nginx/sites-enabled/vagrant-nginx.conf
 
 sudo rm /etc/nginx/sites-enabled/default
 
